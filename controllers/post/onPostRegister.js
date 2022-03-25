d5d5a351a611560c6b802897d377b12cec7d87f3
Mark <mark.schutte@hva.nl>
2022-03-25 16:15:47 +0100

    eerste opzet nodemailer

--- controllers/post/onPostRegister.js
+++ controllers/post/onPostRegister.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../../models/User.js')
+const nodemailer = require('nodemailer')
 
 async function onPostRegister(req, res) {
 	//res.render('register');
@@ -20,6 +21,40 @@ async function onPostRegister(req, res) {
 			password: hashedPassword,
 			pokemon: req.body.pokemon
 		})
+
+		let transporter = nodemailer.createTransport({
+			host: 'smtp.ethereal.email',
+			port: 587,
+			secure: false, // true for 465, false for other ports
+			auth: {
+				user: req.body.name, // generated ethereal user
+				pass: req.body.password, // generated ethereal password
+			},
+		})
+		let url = 'http://localhost:1337/login'
+		if(process.env.PRODUCTION) {
+			url = ''//heroku url
+		}
+		let info = await transporter.sendMail({
+			from: '"Pokemon Match 🔥" <register@pokemonmatch.com>', // sender address
+			to: req.body.email, // list of receivers
+			subject: 'Welcome to Pokemon Match!', // Subject line
+			//text: 'Hello world?', // plain text body
+			html: `<h1>Hello ${req.body.name}</h1>
+			<h2>Thanks for signing up!</h2>
+			<p> You can now <a href="${url}">login here!</a></p>
+			<p> Here is your login information: </p>
+			<ul>
+				<li><b> Email: </b> ${req.body.email}</li>
+				<li><b> Password: </b> ${req.body.password} </li>
+			 </ul>
+			 <p> Enjoy your time and have fun! </p>
+			` // html body
+		})
+
+		
+
+
 		res.redirect('/login')
        
 	} catch(err) {
