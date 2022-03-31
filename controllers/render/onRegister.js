c0dc6a14947a2ae1ac7a6ba910b2ecbd9c5f3030
Mark <mark.schutte@hva.nl>
2022-03-31 14:17:03 +0200

    Making webapi pokemon

--- controllers/render/onRegister.js
+++ controllers/render/onRegister.js
@@ -1,5 +1,27 @@
-function onRegister(req, res) {
-	res.render('register')
+const fetch = require('node-fetch')
+async function onRegister(req, res) {
+	//voor de apifunctie
+	//Fetch de gegevens van alleen 1 pokemon, in dit geval bulbasaur
+	//Gegevens zijn van alles en nogwat (afbeeldingen, moves, generatie en nog meer)
+	const responseBulbasaur = await fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+	const responseCharmander = await fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+	const responseSquirtle = await fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
+	//Wacht totdat gegevens geladen zijn
+	const dataBulbasaur = await responseBulbasaur.json()
+	const dataCharmander = await responseCharmander.json()
+	const dataSquirtle = await responseSquirtle.json()
+
+	//Kijken of de image meegaat
+	console.log(dataBulbasaur.sprites.front_default)
+	console.log(dataCharmander.sprites.front_default)
+	console.log(dataSquirtle.sprites.front_default)
+
+	res.render('register', {
+		//Verstuur de data naar register form.
+		bulbasaur: dataBulbasaur,
+		charmander: dataCharmander,
+		squirtle: dataSquirtle
+	})
 }
 
 module.exports = onRegister
